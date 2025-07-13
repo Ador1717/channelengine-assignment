2d6abab1441f1f79476fb06d54ef244aae66e65b
Ador Negash <adordawit4@gmail.com>
2025-07-14 00:03:57 +0200

    Removed unused ProductDto, updated stock DTO

--- ChannelEngine.Shared/obj/Debug/net9.0/ChannelEngine.Shared.AssemblyInfo.cs
+++ ChannelEngine.Shared/obj/Debug/net9.0/ChannelEngine.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChannelEngine.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eacc1151fefc890d3bd49c33e4341a919309ee07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccc8a6ac53ccae8a6676d6cb92cca073e60291e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChannelEngine.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChannelEngine.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
